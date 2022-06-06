a5b591d99b8d5c38054e47d6fb228711b437e264
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2022-06-06 20:13:46 +0700

    a

--- js/0.js
+++ js/0.js
@@ -1,1 +1,19 @@
-console.log("Hello world")
+console.log("Hello world");
+//in-line comment
+/*
+multi line comment
+*/
+
+/*Data Types
+undefined, null, booleen, string, symbol, number, object
+*/
+
+var myname = "ARRGGGHHH";
+myname = 1;
+
+let ourname = "GGGG";
+
+const pi = 3.14;
+
+var a;
+var b=2;

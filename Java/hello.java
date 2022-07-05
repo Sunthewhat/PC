1ba8af0665a4002a9f7d2f7debf32160710eaaf9
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2022-07-05 20:32:25 +0700

    a

--- Java/hello.java
+++ Java/hello.java
@@ -1,6 +1,7 @@
-package Java;
-public class hello {
+package java;
+
+public class hello {    
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("Hello");
     }
 }

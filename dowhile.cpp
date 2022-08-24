18bee472a2bf6185ed786aac0aff1bbb10f53e92
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2022-08-24 21:00:10 +0700

    a

---
+++ dowhile.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define setp(n) fixed<<setprecision(n)
+using namespace std;
+main() {
+    ios::sync_with_stdio(0);cin.tie(0);
+    int a,i=1,j=0;
+    cin>>a;
+    while (a--) {
+        j=i;
+        while (j--)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+        i++;
+    }
+    return 0;
+}

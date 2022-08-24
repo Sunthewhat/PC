91ad1984471b0d6f14bd3a3bea15468825805891
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2022-08-24 21:36:24 +0700

    a

--- dowhile.cpp
+++ dowhile.cpp
@@ -4,14 +4,14 @@ using namespace std;
 main() {
     ios::sync_with_stdio(0);cin.tie(0);
     int a,i=1,j=0;
-    cin>>a;
+    scanf("%d",&a);
     while (a--) {
         j=i;
         while (j--)
         {
-            cout<<"*";
+            printf("*");
         }
-        cout<<"\n";
+        printf("\n");
         i++;
     }
     return 0;
